90573d6dd4c5d32cb1a71ba76e832aeea3d53da7
ezgikardes <ezgikardes@gmail.com>
2025-01-19 21:35:56 +0100

    feat: add form submission and button click functionality

--- src/FormAddFriend.js
+++ src/FormAddFriend.js
@@ -1,12 +1,43 @@
+import { useState } from "react";
 import Button from "./Button";
 
 export default function FormAddFriend() {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("https://i.pravatar.cc/48");
+
+  function handleChangeName(e) {
+    setName(e.target.value);
+  }
+
+  function handleChangeImage(e) {
+    setImage(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!name || !image) return;
+    const id = crypto.randomUUID();
+
+    const newFriend = {
+      id,
+      name,
+      image: `${image}?=${id}`,
+      balance: 0,
+    };
+
+    console.log(newFriend);
+
+    setName("");
+    setImage("https://i.pravatar.cc/48");
+  }
+
   return (
-    <form className="form-add-friend">
+    <form className="form-add-friend" onSubmit={handleSubmit}>
       <label>👫 Friend name</label>
-      <input type="text" />
+      <input type="text" value={name} onChange={(e) => handleChangeName(e)} />
       <label>📷 Image URL</label>
-      <input type="text" />
+      <input type="text" value={image} onChange={(e) => handleChangeImage(e)} />
       <Button>Add</Button>
     </form>
   );
